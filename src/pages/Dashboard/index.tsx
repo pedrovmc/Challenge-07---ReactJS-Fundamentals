8eb5a5cedc31eed48f60d9e55ba9379a88157d9a
Pedro Carvalho <pedrovmc_@pop-os.localdomain>
2020-12-15 08:44:34 -0300

    first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,127 @@
+/* eslint-disable no-param-reassign */
+import React, { useState, useEffect } from 'react';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const response = await api.get('transactions');
+      console.log(response.data);
+      const transactionsList: Transaction[] = response.data.transactions;
+
+      const balanceData: Balance = response.data.balance;
+      balanceData.income = formatValue(Number(balanceData.income));
+      balanceData.outcome = formatValue(Number(balanceData.outcome));
+      balanceData.total = formatValue(Number(balanceData.total));
+
+      setBalance(balanceData);
+      console.log(response.data.balance);
+
+      transactionsList.forEach(transaction => {
+        transaction.formattedValue = formatValue(transaction.value);
+        if (transaction.type === 'outcome') {
+          transaction.formattedValue = `- ${transaction.formattedValue}`;
+        }
+        const notFormatedDateString = transaction.created_at.toString();
+        const ano = notFormatedDateString.slice(0, 4);
+        const mes = notFormatedDateString.slice(5, 7);
+        const dia = notFormatedDateString.slice(8, 10);
+        transaction.formattedDate = `${dia}/${mes}/${ano}`;
+      });
+
+      setTransactions(transactionsList);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{balance.income}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{balance.total}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
